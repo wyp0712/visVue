4a26b1fba36c6e48af6b52da6e20c0aa3ee01e83
devin-wang <wangyipeng@360.net>
2018-06-26 21:30:10 +0800

    third

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/visbase',
+      name: 'visbase',
+      component: resolve => require(['@/views/dynamicComponent/index.vue'], resolve)
     }
   ]
 })
--- src/views/dynamicComponent/index.vue
+++ src/views/dynamicComponent/index.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="dynamic-components_container-demo">
-
+    {{test}}
   </div>
 </template>
 <script>
   export default {
     name: '',
     data() {
-      
+      return {
+        test: 'test'
+      }
     }
   }
 </script>
